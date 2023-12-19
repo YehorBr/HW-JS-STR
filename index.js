87bd5071aa6b3f523ad2da608ebf5c9a6a009e8d
Yehor <yehor12344321@gmail.com>
2023-12-19 19:01:36 +0200

    зберігає зміни

---
+++ index.js
@@ -0,0 +1,20 @@
+// const result = 5 + 5 + "5";
+// console.log(result);
+
+
+// const email = "fdipfaip@gmail.com";
+
+// console.log(email.includes("@"));
+// console.log(email.length);
+
+// const first = "My ";
+// const second = "Name ";
+// const third = "is ";
+
+// const fullName = first + second + third + "Victor";
+// console.log(fullName);
+
+// const userName  = "Єгоре";
+// const payment = "100";
+
+// alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
